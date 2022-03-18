8d3f9984af8cdb8f09aebfa33a4f8ff3c55c51be
Andrew <metalbest@rambler.ru>
2022-03-18 09:59:08 +0300

    /

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
 let d = new Date();
 document.body.innerHTML = "<p class = 'time'>" + d + "</p>"
-
-let user = new Object(); // синтаксис "конструктор объекта"
-let user = {
-    name: "Andrew",
-    age: 36
-};  // синтаксис "литерал объекта", литеральный синтаксис
-
-var arrObjects = []; // объявляем массив
-
-arrObjects[0] = {
-    id: "1",
-    name: "firstArrElement"
-}
-arrObjects[1] = {
-    id: "2",
-    name: "secondArrElement"
-}
