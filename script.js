73a8cf4730387e44c6cbd8577447001687a720af
Andrew <metalbest@rambler.ru>
2022-04-14 22:05:02 +0300

    /

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+
+
+
 let d = new Date();
-document.body.innerHTML = "<p class = 'time'>" + d + "</p>"
+document.body.innerHTML = "<h3>" + d + "</h3>"
+
+
+
+var a = new Array("Привет всем!", 7, true);
+document.body.innerHTML = `<th>${a[1]}</th>`
+
+const friends = ['petya', 'vasya', 'ivan'];
+document.body.innerHTML = `<p>${friends[2]}</p>`
