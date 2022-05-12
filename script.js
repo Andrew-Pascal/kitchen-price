e3da78bdbbcb1a22b6b145e56ad476d2ab65a8f1
Andrew <metalbest@rambler.ru>
2022-05-12 09:53:18 +0300

    /

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 let d = new Date();
 document.body.innerHTML = "<h2 class='time'>" + d + "</h2>"
+
+
+$('.coloring td').click(function()    {
+    //if click on checkbox cell
+    var $flag = $("input", $(this));
+    //if click on cell without checkbox
+    if($flag.html() == null)    {
+        $flag = $("input", $(this).parent());
+    }
+    var checked = $flag.attr('checked');
+    //if checkbox is not checked
+    if(checked == false)    {
+        $flag.attr("checked","checked");
+    }
+    //checkbox is checked
+    else    {
+        $flag.removeAttr('checked');
+    }
+}); 
